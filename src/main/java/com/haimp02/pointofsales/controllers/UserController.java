6cc20c45fb2fe55da2ea20f592b98ac83a42091d
syarifmuhammad <syarifmuhammad369@gmail.com>
2022-03-03 13:56:00 +0800

    add user controller

---
+++ src/main/java/com/haimp02/pointofsales/controllers/UserController.java
@@ -0,0 +1,74 @@
+package com.haimp02.pointofsales.controllers;
+
+import com.haimp02.pointofsales.models.entities.User;
+import com.haimp02.pointofsales.services.interfaces.SecurityService;
+import com.haimp02.pointofsales.services.interfaces.UserService;
+import com.haimp02.pointofsales.validators.UserValidator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private SecurityService securityService;
+
+    @Autowired
+    private UserValidator userValidator;
+
+    @RequestMapping("/login")
+    public String login(Model model, String error, String logout) {
+        if (securityService.isAuthenticated()) {
+            return "redirect:/";
+        }
+
+        if (error != null)
+            model.addAttribute("error", "Your email and password is invalid.");
+
+        if (logout != null)
+            model.addAttribute("message", "You have been logged out successfully.");
+
+        return "login";
+    }
+
+
+    // Route untuk direct ke halaman
+
+    @GetMapping("/users")
+    public String index(Model model) {
+        model.addAttribute("users", userService.findAll());
+
+        return "users/index";
+    }
+
+    @GetMapping("/users/create")
+    public String create(Model model) {
+        model.addAttribute("userForm", new User());
+
+        return "users/create";
+    }
+
+
+    //route method post 
+
+    @PostMapping("/users/create")
+    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult) {
+        userValidator.validate(userForm, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "users/create";
+        }
+        userService.save(userForm);
+        return "redirect:/";
+    }
+}
