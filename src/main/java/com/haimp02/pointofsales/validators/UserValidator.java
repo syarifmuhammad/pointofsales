14d4859ae110229b84df98450a52c2594e164178
syarifmuhammad <syarifmuhammad369@gmail.com>
2022-03-03 13:52:13 +0800

    make user validator request

---
+++ src/main/java/com/haimp02/pointofsales/validators/UserValidator.java
@@ -0,0 +1,44 @@
+package com.haimp02.pointofsales.validators;
+
+import com.haimp02.pointofsales.models.entities.User;
+import com.haimp02.pointofsales.services.interfaces.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+@Component
+public class UserValidator implements Validator {
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return User.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        User user = (User) o;
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "fullname", "NotEmpty");
+        if (user.getFullname().length() > 100) {
+            errors.rejectValue("fullname", "Size.userForm.fullname");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "NotEmpty");
+        if (user.getEmail().length() < 8 || user.getEmail().length() > 32) {
+            errors.rejectValue("email", "Size.userForm.email");
+        }
+        if (userService.findByEmail(user.getEmail()) != null) {
+            errors.rejectValue("email", "Duplicate.userForm.email");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty");
+        if (user.getPassword().length() < 8 || user.getPassword().length() > 32) {
+            errors.rejectValue("password", "Size.userForm.password");
+        }
+    }
+}
