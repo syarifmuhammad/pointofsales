8caed9198791112db0a153a2a9233a00b57e9a54
syarifmuhammad <syarifmuhammad369@gmail.com>
2022-03-04 11:32:05 +0800

    add product and category entity

---
+++ src/main/java/com/haimp02/pointofsales/models/entities/Product.java
@@ -0,0 +1,74 @@
+package com.haimp02.pointofsales.models.entities;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Double purchase_price;
+
+    @Column(nullable = false)
+    private Double selling_price;
+
+    @ManyToOne()
+    @JoinColumn(name="category_id")
+    private Category category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPurchase_price() {
+        return purchase_price;
+    }
+
+    public void setPurchase_price(Double purchase_price) {
+        this.purchase_price = purchase_price;
+    }
+
+    public Double getSelling_price() {
+        return selling_price;
+    }
+
+    public void setSelling_price(Double selling_price) {
+        this.selling_price = selling_price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    
+}
