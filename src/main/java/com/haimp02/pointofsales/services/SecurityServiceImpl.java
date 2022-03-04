1f4a34df49b377c0c2a244d6806c193abc04a2a5
syarifmuhammad <syarifmuhammad369@gmail.com>
2022-03-04 11:31:43 +0800

    add service auto login (belum berhasil)

--- src/main/java/com/haimp02/pointofsales/services/SecurityServiceImpl.java
+++ src/main/java/com/haimp02/pointofsales/services/SecurityServiceImpl.java
@@ -7,8 +7,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
@@ -24,10 +26,17 @@ public class SecurityServiceImpl implements SecurityService {
 
     public boolean isAuthenticated() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || AnonymousAuthenticationToken.class.
-            isAssignableFrom(authentication.getClass())) {
+        if (authentication == null || AnonymousAuthenticationToken.class.isAssignableFrom(authentication.getClass())) {
             return false;
         }
         return authentication.isAuthenticated();
+    }
+
+    @Override
+    public void autoLogin(String email, String password) {
+        Authentication request = new UsernamePasswordAuthenticationToken(email, password);
+        Authentication result = authenticationManager.authenticate(request);
+        SecurityContextHolder.getContext().setAuthentication(result);
+
     }
 }
--- src/main/java/com/haimp02/pointofsales/services/interfaces/SecurityService.java
+++ src/main/java/com/haimp02/pointofsales/services/interfaces/SecurityService.java
@@ -2,4 +2,5 @@ package com.haimp02.pointofsales.services.interfaces;
 
 public interface SecurityService {
     boolean isAuthenticated();
+    void autoLogin(String email, String password);
 }
